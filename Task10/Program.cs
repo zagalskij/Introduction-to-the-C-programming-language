ebabb4891da435ba2ed3c131702fd7ff7838c3d4
Anatoliy <zaghalskiy@mail.ru>
2023-01-19 22:31:29 +0300

    all task complete

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+int SecondNumber(int a)
+{
+    return a % 100 / 10;
+
+}
+Console.WriteLine("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("You entered a non-three-digit number");
+}
+else
+    Console.WriteLine("The second number:" + SecondNumber(number));
---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int ThirdNumber(int a)
+{
+    return a%100%10;
+
+}
+Console.WriteLine("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("You entered a non-three-digit number");
+}
+else
+    Console.WriteLine("The third number:" + ThirdNumber(number));
