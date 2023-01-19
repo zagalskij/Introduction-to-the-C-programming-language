4a4ec477d328b4aab4281ada41d4a6e7202120a2
Anatoliy <zaghalskiy@mail.ru>
2023-01-19 21:16:41 +0300

    does not work

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// цифру, обозначающую день недели, и проверяет, является ли 
+// этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+string DayOff(int number)
+{
+    if (number < 1 && number > 7)
+        return "You entered the wrong number!";
+    else if (number > 5)
+        return "Yes, this day is a day off!";
+    else
+        return "no this day is not a day off!";
+}
+Console.WriteLine("Enter the number of the day of the week from 1 to 7: " + DayOff(int.Parse(Console.ReadLine)));
