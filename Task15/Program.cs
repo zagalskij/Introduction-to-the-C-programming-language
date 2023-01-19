873791b81a85356d8f19358f2296339eb10b11fe
Anatoliy <zaghalskiy@mail.ru>
2023-01-19 22:29:42 +0300

    all task complete

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -6,11 +6,12 @@
 // 1 -> нет
 string DayOff(int number)
 {
-    if (number < 1 && number > 7)
+    if (number < 1 || number > 7)
         return "You entered the wrong number!";
     else if (number > 5)
         return "Yes, this day is a day off!";
     else
         return "no this day is not a day off!";
 }
-Console.WriteLine("Enter the number of the day of the week from 1 to 7: " + DayOff(int.Parse(Console.ReadLine)));
+Console.WriteLine("Enter the number of the day of the week from 1 to 7: ");
+Console.WriteLine(DayOff(int.Parse(Console.ReadLine())));
