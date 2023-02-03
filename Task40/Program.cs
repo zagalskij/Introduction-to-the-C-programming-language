83e30fbb725c5d50235a10eafa7ed05286e9084e
Anatoliy <zaghalskiy@mail.ru>
2023-02-03 08:03:50 +0300

    started task 42

---
+++ Task40/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа и проверяет,
+//может ли существоваит треугольник с сторонами такой длины.
+
+bool TheExistenceOfATriangle(double a, double b, double c)
+{
+    if (a < b + c && b < a + c && c < b + a)
+        return true;
+    else
+        return false;
+}
+Console.WriteLine("enter side A");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("enter side B");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("enter side C");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine("TheTriangleExists :" + TheExistenceOfATriangle(a, b, c));
---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10 
+
+int[] FillArray(int number)
+{
+    int[] mas = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write("Enter the " + i + " element: ");
+        mas[i] = int.Parse(Console.ReadLine());
+    }
+    return mas;
+}
+
+int FromDecimalToBinary(int number)
+{
+
+if (number ==0)
+return 0;
+number%2;
+return FromDecimalToBinary(number/2);
+
+}
+Console.WriteLine(FromDecimalToBinary(45));
