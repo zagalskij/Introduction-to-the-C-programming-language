b56cfee6a4748592fe395ba3cb1c697fdd77ab92
Anatoliy <zaghalskiy@mail.ru>
2023-02-09 07:57:45 +0300

    Complete Task47

---
+++ Task47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными 
+// вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+double[,] FillArray(int columns, int rows, double min, double max)
+{
+    Random rand = new Random();
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(min + rand.NextDouble() * (max - min), 2);
+        }
+    }
+    return array;
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Enter the number of columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the minimum element in the array");
+double min = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the maximum element in the array");
+double max = Convert.ToDouble(Console.ReadLine());
+PrintArray(FillArray(columns,rows,min,max));
---
+++ Task59/Program.cs
@@ -0,0 +1,45 @@
+﻿// //  Задайте двумерный массив из целых чисел. 
+// Напишите программу, которая удалит строку и столбец, 
+Console.Write("Ввести размер m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввести размер n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] FillArray(int m, int n)
+{
+int[,] arr = new int[m, n];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = arr[i, j] = new Random().Next(-9, 10);
+    }
+}
+return arr;
+}
+
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] FindPositionSmallElement(int[,] array)
+{
+  int temp = array[0, 0];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] <= temp)
+      {
+        position[0, 0] = i;
+        position[0, 1] = j;
+        temp = array[i, j];
+      }
+    }
