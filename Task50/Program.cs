0c1e9d9b8325f6b65fe622905a9269e89c26a51a
Anatoliy <zaghalskiy@mail.ru>
2023-02-10 19:24:51 +0300

    Complete Task47, Task50, Task52

---
+++ Task50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или 
+// же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+// 1 1 -> 9
+double[,] FillArray(int columns, int rows)
+{
+    double[,] array = new double[columns, rows];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            Console.Write("Enter array["+i+","+j+"] : ");
+            array[i, j] = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    return array;
+}
+void FindNumber(double[,] array, int columns, int rows)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (columns == i && rows == j)
+            {
+                Console.WriteLine("array["+i+","+j+"] : "+array[i, j]);
+                return;
+            }
+        }
+    }
+    Console.WriteLine("There is no such element");
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("enter the number of columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+double[,] array = FillArray(columns, rows);
+Console.WriteLine("Enter the column number");
+int IndexColumns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the row number");
+int IndexRows = Convert.ToInt32(Console.ReadLine());
+FindNumber(array,IndexColumns,IndexRows);
---
+++ Task52/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int[,] FillArray(int columns, int rows)
+{
+    int[,] array = new int[columns, rows];
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            Console.Write("Enter array["+i+","+j+"] : ");
+            array[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    return array;
+}
+void ArithmeticMean(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+          double sum=0;
+        for (int i=0 ; i < array.GetLength(1); i++)
+        {
+            sum += array[i, j];
+        }
+                    Console.Write(sum/array.GetLength(0)+"; ");
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("enter the number of columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillArray(columns, rows);
+PrintArray(array);
+Console.WriteLine("The arithmetic mean of each number: ");
+ArithmeticMean(array);
